d69917cc3c52e33ebae7bd891aec2f321ffe817d
Pawel Walesiak <walesiakpawel@gmail.com>
2023-05-04 21:32:52 +0200

    Add metriccalc tests

---
+++ MyProject.Tests/MetricCalculatorBmiTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject.Tests
+{
+    public class MetricCalculatorBmiTests
+    {
+        [Theory]
+        [InlineData(100, 170, 34.6)]
+        [InlineData(57, 170, 19.72)]
+        [InlineData(70, 170, 24.22)]
+        public void CalculateBmi_ForgivenWightAndHeidht_ReturnsCorrectBmi(double weight, double height, double bmiResult)
+        { 
+            //arragne
+            MetricBmiCalculator metricBmiCalculator = new MetricBmiCalculator();
+
+            //act 
+
+            double result = metricBmiCalculator.CalculateBmi(weight, height);
+            
+            //assert
+            Assert.Equal(bmiResult, result);
+        }
+    }
+}
